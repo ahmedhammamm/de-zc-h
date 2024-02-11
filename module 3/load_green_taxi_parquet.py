950a94627320e3178947565e86ea80c42a7e97b5
Ahmed Hammam <ahmedhammam710@gmail.com>
2024-02-11 22:58:23 +0200

    week 3 homework compelted

---
+++ module 3/load_green_taxi_parquet.py
@@ -0,0 +1,40 @@
+import io
+import pandas as pd
+import requests
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    urls = [
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-01.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-02.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-03.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-04.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-05.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-06.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-07.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-08.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-09.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-10.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-11.parquet',
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-12.parquet',
+    ]
+    
+   
+    final_df = pd.DataFrame()
+
+    for url in urls:
+        df = pd.read_parquet(url)
+        df['lpep_pickup_date'] = df['lpep_pickup_datetime'].dt.date
+        final_df = pd.concat([final_df, df], ignore_index=True)
+
+
+    return final_df
